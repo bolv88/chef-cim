825f813114f0420ab6b9f37fc14fb52e8fcdb3c9
root <root@cim120.backup>
2014-09-02 11:38:34 +0800

    --

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -8,4 +8,18 @@
 #
 message = node['my_cookbook']['message']
 Chef::Log.info("** Saying what I was told to say: #{message}")
+
+template '/tmp/index.html' do
+  source 'index.html.erb'
+  variables(hi: 'hello', word: message, from: node['fqdn'])
+end
+
+capistrano_deploy_dirs "cap cim120 dirs" do
+	deploy_to "/srv"
+end
+
+my_cookbook "Ohai" do
+	title "Chef"
+	action :remove
+end
 #hehe
