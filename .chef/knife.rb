e60738a2c892afbd6c793312bf2df30ebae4c3a9
Yu Bo <bolv88@gmail.com>
2014-09-11 17:51:05 +0800

    delete file

--- .chef/knife.rb
+++
@@ -1,9 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'yubo'
-client_key               '/root/chef-cim/.chef/yubo.pem'
-validation_client_name   'chef-validator'
-validation_key           '/root/chef-cim/.chef/chef-validator.pem'
-chef_server_url          'https://server:443'
-syntax_check_cache_path  '/root/chef-cim/.chef/syntax_check_cache'
-cookbook_path [ '/root/chef-cim/cookbooks/']
