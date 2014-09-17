333489423fd4b256d232092fd9e9d95ea013916a
root <root@cim120.backup>
2014-09-17 17:37:05 +0800

    add sysctl

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,77 +6,73 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-message = node['my_cookbook']['message']
-Chef::Log.info("** Saying what I was told to say: #{message}")
-
-template '/tmp/index.html' do
-  source 'index.html.erb'
-  variables(hi: 'hello', word: message, from: node['fqdn'])
-end
-
-capistrano_deploy_dirs "cap cim120 dirs" do
-	deploy_to "/srv"
-end
-
-my_cookbook "Ohai" do
-	title "Chef"
-	action :remove
-end
-
-#yum install
-include_recipe "yum"
-yum_repository "ius" do
-  description 'ius'
-  mirrorlist 'http://dmirr.iuscommunity.org/mirrorlist?repo=ius-el6&arch=$basearch'
-  gpgkey 'http://mirror.its.dal.ca/ius/IUS-COMMUNITY-GPG-KEY'
-  action :create
-end
-
-#package "libcurl"
-#package "libcurl-devel"
-#yum_package "php54"
-
-
-
-#users
-include_recipe "users"
-users_manage "staff" do
-  group_id 999
-  action [:create]
-end
-
-#sudo
-#node.default['openssh']['server']['listen_address'] = '192.168.1.1'
-#node.default['openssh']['server']['port'] = '6222'
+# common settings like sysctl
 #
-#node.default['openssh']['server']['permit_root_login'] = "no"
-#node.default['openssh']['server']['password_authentication'] = "no"
+# sysctl
+net = {}
+net['ipv4'] = {}
+net['core'] = {}
+#port range
+net['ipv4']['ip_local_port_range'] = "10000 61000"
+
+# increase TCP max buffer size settable using setsockopt()
+net['core']['rmem_max'] = 16777216 
+net['core']['wmem_max'] = 16777216 
 
 
-node.default['authorization']['sudo']['passwordless'] = true
-node.default['authorization']['sudo']['groups'] = ['staff']
+# increase Linux autotuning TCP buffer limit 
+net['ipv4']['tcp_rmem'] = "4096 87380 16777216"
+net['ipv4']['tcp_wmem'] = "4096 65536 16777216"
 
-include_recipe 'sudo'
+# increase the length of the processor input queue
+net['core']['netdev_max_backlog'] = 30000
 
-#include_recipe 'ntp'
-#include_recipe 'ntp::undo'
-Chef::Log.info("before varnish")
+# recommended default congestion control is htcp 
+net['ipv4']['tcp_congestion_control']="htcp"
 
-#node.default['varnish']['backend_port'] = '80'
-#include_recipe 'varnish'
+# recommended for hosts with jumbo frames enabled
+net['ipv4']['tcp_mtu_probing']=1
 
-#yum_package "nginx"
-#include_recipe "my_cookbook::nginx_config"
+#默认60，tcp fin状态超时时间 
+net['ipv4']['tcp_fin_timeout'] = 10
 
-yum_package "haproxy"
-include_recipe "my_cookbook::haproxy_config"
+#tcp重用
+net['ipv4']['tcp_tw_reuse'] = 1
+#打开可能回引发一些奇怪的问题
+net['ipv4']['tcp_tw_recycle'] = 0
+net['ipv4']['tcp_syncookies']=0
 
-include_recipe "my_cookbook::redis"
+net['ipv4']['tcp_no_metrics_save']=1
 
-#node.default['haproxy']['httpchk'] = true
-#node.default['haproxy']['x_forwarded_for'] = true
-#node.default['haproxy']['app_server_role'] = "web_servers"
-#node.default['haproxy']['incoming_port'] = 80
-#node.default['haproxy']['member_port'] = 8081
-#include_recipe "haproxy::app_lb"
+#listen()的默认参数,挂起请求的最大数量.默认是128.对繁忙的服务器,增加该值有助于网络性能.可调整到256.
+net['core']['somaxconn'] = 1024
+
+#系统所能处理不属于任何进程的TCP sockets最大数量。假如超过这个数量﹐那么不属于任何进程的连接会被立即reset，并同时显示警告信息。之所以要设定这个限制﹐纯粹为了抵御那些简单的 DoS 攻击﹐千万不要依赖这个或是人为的降低这个限制(这个值Redhat AS版本中设置为32768,但是很多防火墙修改的时候,建议该值修改为2000)
+net['ipv4']['tcp_max_orphans'] = 262144
+
+#对于那些依然还未获得客户端确认的连接请求﹐需要保存在队列中最大数目。对于超过 128Mb 内存的系统﹐默认值是1024 ﹐低于 128Mb 的则为 128。如果服务器经常出现过载﹐可以尝试增加这个数字。警告﹗假如您将此值设为大于1024﹐最好修改 include/net/tcp.h 里面的 TCP_SYNQ_HSIZE ﹐以保持TCP_SYNQ_HSIZE*16<=tcp_max_syn_backlog ﹐并且编进核心之内。(SYN Flood攻击利用TCP协议散布握手的缺陷，伪造虚假源IP地址发送大量TCP-SYN半打开连接到目标系统，最终导致目标系统Socket队列资源耗尽而无法接受新的连接。为了应付这种攻击，现代Unix系统中普遍采用多连接队列处理的方式来缓冲(而不是解决)这种攻击，是用一个基本队列处理正常的完全连接应用(Connect()和Accept() )，是用另一个队列单独存放半打开连接。这种双队列处理方式和其他一些系统内核措施(例如Syn-Cookies/Caches)联合应用时，能够比较有效的缓解小规模的SYN Flood攻击(事实证明<1000p/s)加大SYN队列长度可以容纳更多等待连接的网络连接数，所以对Server来说可以考虑增大该值.)
+net['ipv4']['tcp_max_syn_backlog'] = 262144
+
+#受到攻击时可设置该值
+#net['ipv4']['tcp_synack_retries = 2
+
+net['ipv4']['tcp_syn_retries']=2
+
+# net['ipv4']['tcp_keepalive_intvl = 75
+# net['ipv4']['tcp_keepalive_probes = 9
+# net['ipv4']['tcp_keepalive_time = 7200
+# 意思是如果某个TCP连接在idle 7200秒（2个小时）后,内核才发起probe.如果probe 9次(每次75秒)不成功,内核才彻底放弃,认为该连接已失效
+net['ipv4']['tcp_keepalive_time'] = 300
+net['ipv4']['tcp_keepalive_intvl'] = 75
+net['ipv4']['tcp_keepalive_probes'] = 9
+
+params = {}
+params['net'] = net
+params['fs'] = {}
+params['fs']['file-max'] = 6553560
+node.default['sysctl']['params']=params
+
+include_recipe 'sysctl::apply'
+
+
 
--- roles/web_server.rb
+++ roles/web_server.rb
@@ -1,6 +1,6 @@
 name "web_server"
 description "web server [nginx php]"
-run_list "recipe[my_cookbook::web_server]" #, "recipe[ntp]"
+run_list "recipe[my_cookbook::web_server]", "recipe[my_cookbook]" #, "recipe[ntp]"
 default_attributes "ntp" => {"servers" => ["0.pool.ntp.org", "1.pool.ntp.org"]}
 
 
